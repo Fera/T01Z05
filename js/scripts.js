13f664c421890257258af70fa852c85a7dbcce32
Joanna Bieńkowska <ferart.pl@gmail.com>
2017-03-11 12:48:52 +0100

    optymalizacja kodu

--- js/scripts.js
+++ js/scripts.js
@@ -10,13 +10,16 @@
             output = $("#output"); // zmienna odnosząca do elementu o podanym id
             btnclear = $(".clear"); // zmienn odnosząca do elementu o podanej klasie
 
-        $(output).hide();
+        output.hide();
 
         button.on("click", function() { // po kliknięciu
+
+
 
             var that = $(this);
 
             that.attr('disabled', true).text("DANE POBRANO"); // wyłącz przycisk pobierania danych i zmień mu text   
+
 
             $.ajax({ // korzystamy z metody ajax
                 url: "https://jsonplaceholder.typicode.com/users", //wysyłamy zapytanie pod podany url
@@ -25,34 +28,36 @@
                
                 success: function(response){ // w razie sukcesu ma zadziałać taka funkcja
 
-                    $.each(response, function(i,value){ // weź wszystkie odpowiedzi wg indexu i wartości
+                    $.each(response, function(i,val){ // weź wszystkie odpowiedzi wg indexu i wartości
 
-                        var name = '<span class="name">' + value.name + '</span>',
-                            username = '<span class="username">' + value.username + '</span>',
-                            phone = '<span class="phone">' + value.phone + '</span>',
-                            email = '<span class="email">' + value.email + '</span>',
+                        var name = '<span class="name">' + val.name + '</span>',
+                            username = '<span class="username">' + val.username + '</span>',
+                            phone = '<span class="phone">' + val.phone + '</span>',
+                            email = '<span class="email">' + val.email + '</span>',
                             data = name + username + phone + email,
                             li = '<li>' + data + '</li>';
 
                         $(li).appendTo(output); // to wszystko wstaw do środka elementu output
                     });                   
 
-                    $(output).fadeIn(500, 'linear');  // pokaż output
+                    output.fadeIn(500, 'linear');  // pokaż output
                 },
 
                 error: function(errorThrown) { // w razie błędu ma się to zadziać
-                    $(output).fadeIn(500, 'linear').html("<li class='error'>Przepraszamy, wystąpił błąd</li>"); // pojawi się pozycja na liście z informacją o błędzie
+                    output.fadeIn(500, 'linear').html("<li class='error'>Przepraszamy, wystąpił błąd</li>"); // pojawi się pozycja na liście z informacją o błędzie
                     that.text("SPRÓBUJ JESZCZE RAZ"); // zmien tekst buttona
                 }
-            });         
+            }); 
+
+             // czyszczenie listy po kliknięciu na button clear i włączenie przycisku pobierania danych
+
+            btnclear.on("click", function(){
+                output.empty().fadeOut(500, 'linear');
+                button.attr('disabled', false).text("POBIERZ DANE");
+            });        
         });
 
-        // czyszczenie listy po kliknięciu na button clear i włączenie przycisku pobierania danych
-
-        $(btnclear).on("click", function(){
-            $(output).empty().fadeOut(500, 'linear');
-            $(button).attr('disabled', false).text("POBIERZ DANE");
-        });
+        
 
     });
 })(jQuery, window, document);
